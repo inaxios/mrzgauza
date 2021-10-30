bfb42abc62750ccfcedffd2a93c678e065d848ec
inaxios <inakiac@yahoo.es>
2021-10-30 17:04:00 +0200

    komentario zenbait, license.js-a ezabatu, lizentzia berria jarri lokalhost-erako

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -52,6 +52,8 @@ async function myAsyncFunction(){
   console.log("After the delay")
   console.log(document.getElementById('rooti'));
   //Do what you want here too
+  //zergatik horrela? Anyline-k rooti horretan jarri behar ditu bere gauzak. Baino script-a egikaritzean rooti hori ez dago oraindik prest, ta beraz kaskatzen du.
+  //bost segundu itxoiten dugu beraz, rooti-a hor dagoela ziurtatzeko. Gero anyline kargatzen dugu beheko deian.
   theMainThing();
 }
 
@@ -67,7 +69,7 @@ function theMainThing() {
   const Anyline = init({
     preset: 'id',
     viewConfig,
-    license: 'ewogICJsaWNlbnNlS2V5VmVyc2lvbiI6IDIsCiAgImRlYnVnUmVwb3J0aW5nIjogIm9uIiwKICAiaW1hZ2VSZXBvcnRDYWNoaW5nIjogZmFsc2UsCiAgIm1ham9yVmVyc2lvbiI6ICI0IiwKICAibWF4RGF5c05vdFJlcG9ydGVkIjogNSwKICAiYWR2YW5jZWRCYXJjb2RlIjogZmFsc2UsCiAgInBpbmdSZXBvcnRpbmciOiB0cnVlLAogICJwbGF0Zm9ybSI6IFsKICAgICJKUyIKICBdLAogICJzY29wZSI6IFsKICAgICJBTEwiCiAgXSwKICAic2hvd1BvcFVwQWZ0ZXJFeHBpcnkiOiB0cnVlLAogICJzaG93V2F0ZXJtYXJrIjogdHJ1ZSwKICAidG9sZXJhbmNlRGF5cyI6IDUsCiAgInZhbGlkIjogIjIwMjEtMTAtMzEiLAogICJqc0lkZW50aWZpZXIiOiBbCiAgICAicGxhdGUtc2hhcmUuaXRlYy1pbm5vdmF0aW9uLmV1IgogIF0KfQp3VVUvR3loSXJSUDhiZDJMMnZTSFRyVndRU2kwRTVFdHYvQUN3emV5Q2RrSkdCYmZBZUlBOWkyb3BOM0xrY3NUdTZPNHBmYnFxWEYzMWVSNWpOUUt3OVRXcE9VVzI3SUVoQTVHYWxudWZxeWJ4THJZbXFNSTRBVzZ3eFQzZ1VGZXZFZHJEcVpQZHVCcXJPcWNHWDNrVkk0RFI2d1NlVHpleUgrWW9KMlhKMkJ0b1Z5UFY0RS9TUC9LSHNNOW1hcnlFQ2J6c25QcjRNMkxYS0RLdVprZXlEYjUxOWxha0RyOVpxRlNranFvOUpTa1FpUjE0N0libUtmckE4T3JFWElQQm1IOVg2blF0WEVITUIvbGRROWR3NGNCRHNvbmFtN0FMSjF0ajdjSnRTamJGUlQyTWdJTGFNblJjekxtSjN1cHZHbWpwcUdEYkMzekFZK3BhVVAzcEE9PQ==',
+    license: 'eyJzY29wZSI6WyJBTEwiXSwicGxhdGZvcm0iOlsiaU9TIiwiQW5kcm9pZCIsIldpbmRvd3MiLCJKUyIsIldlYiJdLCJ2YWxpZCI6IjIwMjEtMTEtMjciLCJtYWpvclZlcnNpb24iOjMsIm1heERheXNOb3RSZXBvcnRlZCI6NSwic2hvd1dhdGVybWFyayI6dHJ1ZSwicGluZ1JlcG9ydGluZyI6dHJ1ZSwiZGVidWdSZXBvcnRpbmciOiJvbiIsInRvbGVyYW5jZURheXMiOjUsInNob3dQb3BVcEFmdGVyRXhwaXJ5Ijp0cnVlLCJpb3NJZGVudGlmaWVyIjpbImxva2FsaG9zdCJdLCJhbmRyb2lkSWRlbnRpZmllciI6WyJsb2thbGhvc3QiXSwid2luZG93c0lkZW50aWZpZXIiOlsibG9rYWxob3N0Il0sIndlYklkZW50aWZpZXIiOlsibG9rYWxob3N0Il0sImpzSWRlbnRpZmllciI6WyJsb2thbGhvc3QiXSwiaW1hZ2VSZXBvcnRDYWNoaW5nIjp0cnVlLCJsaWNlbnNlS2V5VmVyc2lvbiI6MiwiYWR2YW5jZWRCYXJjb2RlIjp0cnVlLCJzdXBwb3J0ZWRCYXJjb2RlRm9ybWF0cyI6WyJBTEwiXX0KaWFtZXdQVmVNVUdkTlA4d0NIazNGeERSYW0yV3h1QmU2b2lOWllwMEt3TUsySitIbjZrZEw2OUVMT3BYNzRtK252dDY1Q0pmNE1sMkkxME1PdVVzajJMdTZvQXVrS3hLQ1JJV041eHB2bmhlYnVyL0NMaXBlR2VwWmtpSExwRVNpRzVRK2RDYXA0Rk1NdXhwNWJSbm4yb0JsNzV1MVJ0bG9ZY2llZml0eXVDbVpxWmEzN1BvWlVXZ29maDF3cCtQVGtHaDhnQzZ2Ti9WOGVyaHliZy9qRHUxcWNKeTdKV2RIYmdOaXVoTmtJWUxGWTM2TkQzdUVmY1crYnJKRU4xV0pDQ3dPM2FUSWJYcERUUlRaaGJSQWRNYnUwYmRlUHhncW1FeU1WZXpkVDdIQVBrc25lcWNJUDNsYml0U0E2cEVYZm9vOXFYUHZ4L01kYXZtYm85RE53PT0=',
     element: rooti
   });
 
@@ -76,7 +78,7 @@ function theMainThing() {
   Anyline.onResult = function(result) {
     console.log('Anyline has result: ', result);
     console.log('x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x');
-    console.log(result.result[13]);
+    console.log(result.result[14]);
     Anyline.stopScanning();
   };
 }
